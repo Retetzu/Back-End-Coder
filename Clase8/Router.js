f3420645aae8501a154781eba35ad90663b44b7c
Retetzu <javiera208577@gmail.com>
2022-08-28 22:11:23 -0300

    Router.js

--- Clase8/Router.js
+++ Clase8/Router.js
@@ -5,48 +5,43 @@ const app = express();
 
 const productos = Router();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'));
+
+app.use('/', productos)
+
 const listaProductos = []
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}))
-app.use(express.static('public'))
-
-app.get('/api/productos', (req, res) => {
-    res.send(listaProductos)
-  })
-
-app.get('/api/productos/:id', (req, res) => {
-    const busqueda = listaProductos.find(busqueda => busqueda.id === parseInt(req.body.id))
-    res.send(busqueda)
+productos.get('/api/productos', (req, res) => {
+    console.log('get ok')
 })
 
-app.post('/api/productos', (req, res, next) => {
-    const title = req.title
-    if (!title) {
-        res.status(400).send('some data missing')
-    }
-    next()
-},
-    (req, res) => {
-        console.log(req.body);
-        res.send('El producto fue recibido')
-}) 
+productos.post('/api/productos', (req, res, next) => {
+    const {title , price} = req.body
 
-app.put('/api/productos/:id', (req, res, next) => {
-    res.send('post ok')
+    if(!title || !price){        
+      res.status(400).send('producto no encontrado')    
+}
+    next();
+
+}, (req, res) => {
+
+    const {title , price} = req.body
+
+    listaProductos.push({title, price})    
+    console.log(req.body);    
+    res.send('producto guardado con exito')
 })
-
-app.delete('/api/productos/:id', (req, res) => {
-    const deleteado = listaProductos.find(deleteado => deleteado.id === parseInt(req.body.id))
-    if (!deleteado) return res.status(404).send('Producto no encontrado')
-    const index = listaProductos.indexOf(deleteado)
-    listaProductos.splice(index, 1)
-    res.send(deleteado)
-})
-
-app.use('/productos', productos)
 
 const PORT = 8080
 app.listen(PORT, () => {
     console.log('server on');
 })
+
+
+
+// productos.get('/api/productos/:id', (req, res) => {
+//     const busqueda = listaProductos.find(busqueda => busqueda.id === parseInt(req.body.id))
+//     res.send(busqueda)
+// })
